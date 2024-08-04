810761d8a7b9f2c6ffa957c8130c4a58fd123dd9
MarkoPoloMA <mark.utrobin@mail.ru>
2024-08-04 17:14:12 +0400

    4

--- Car.cs
+++ Car.cs
@@ -1,1 +1,18 @@
-﻿internal class Car{}
+﻿using System;
+
+namespace CSharpEducation.Practice3;
+
+internal class Car
+{
+    public string Marka { get; set; }
+
+    public Car(string marka)
+    {
+        Marka = marka;
+    }
+    private void PrintMarkaforCar(Car p1)
+    {
+        Console.WriteLine($"Марка автомобиля: {p1.Marka}");
+    }
+
+}
